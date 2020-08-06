1453374e50a4c8e8a3b7a99774e705ee9822aab9
hiraitakuya <hracchi07@gmail.com>
2020-08-06 17:08:19 +0900

    ユーザー詳細時のインスタンス変数の設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
+    @user = User.find(current_user.id)
+    @products = Product.where(user_id: @user).order(created_at: :desc)
   end
 end
